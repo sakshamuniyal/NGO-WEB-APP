058a7f958c0c676f9429061942a7c47bbac20c41
Saksham Uniyal <sakshamuniyal7@gmail.com>
2026-05-04 22:04:16 +0530

    chore: remove seed files from repository and track in gitignore

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -6,6 +6,9 @@ import dotenv from 'dotenv';
 dotenv.config(); 
 
 export default defineConfig({
+  migrations: {
+    seed: 'npx tsx ./prisma/seed.ts',
+  },
   datasource: {
     url: process.env.DIRECT_URL, 
   },
