5af96a01154c145fd9089b824b97808c3ae98fee
Saksham Uniyal <sakshamuniyal7@gmail.com>
2026-05-11 15:07:47 +0530

    added prisma logs

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,3 +1,4 @@
+console.log("DB URL Check:", process.env.DATABASE_URL ? "Exists (starts with " + process.env.DATABASE_URL.substring(0, 8) + "...)" : "IS COMPLETELY MISSING");
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
